eb401adfc2dcd17c42221e822ca5636f188369e7
Ryan White <ryanwhite_1@yahoo.com.au>
2022-03-18 17:29:55 +1000

    More star clusters, some bug fixes

--- Data/Camera Images/Star Cluster Population Program.py
+++ Data/Camera Images/Star Cluster Population Program.py
@@ -24,7 +24,7 @@ def in_ellipse(xc, yc, a, b, angle, equat, polar):
     '''
     theta = angle * pi / 180            #converts from degrees to radians
     xdist = equat - xc; ydist = polar - yc      #finds separation from point and ellipse center along each axis
-    position = ((xdist * cos(theta) + ydist * sin(theta)) / a)**2 + ((xdist * sin(theta) - ydist * cos(theta)) / b)**2  #finds position of point relative to ellipse
+    position = ((xdist * cos(theta) + ydist * sin(theta)) / (a/2))**2 + ((xdist * sin(theta) - ydist * cos(theta)) / (b/2))**2  #finds position of point relative to ellipse
     if position <= 1:       #if within ellipse:
         return True
     else:
@@ -47,13 +47,22 @@ star_polar = stars['Polar']
 
 for cluster in range(len(clusters)):
     data = []
+    h = 0
+    p = 0
     xcenter = cluster_xlocs[cluster]; ycenter = cluster_ylocs[cluster]; theta = -cluster_angles[cluster] #angle in deg, and negative due to the flipped y-axis (polar coords)
     major = 2 * cluster_x_extents[cluster] / cos(theta*pi/180)  #calculates length of major axis from the x-extent and the angle (converted to radians)
     minor = 2 * cluster_y_extents[cluster] / sin(pi/2 - theta*pi/180)   #calculates length of minor axis from the y-extent and the angle (converted to radians)
     for star in range(len(stars)):
-        if in_ellipse(xcenter, ycenter, major, minor, theta, star_equat[star], star_polar[star]):
+        if (xcenter == 202.5 and ycenter == 140):     #anamolous case for overlapping clusters
+            if in_ellipse(211.4, 143.3, 8.61, 2.27, -28, star_equat[star], star_polar[star]): #checks if star is in visually coincident cluster
+                pass            #if so, does nothing
+            elif in_ellipse(xcenter, ycenter, major, minor, theta, star_equat[star], star_polar[star]):     #checks if star is within parent cluster (the one we care about)
+                data.append(stars.loc[star,:])  #adds the star to parent cluster
+        elif in_ellipse(xcenter, ycenter, major, minor, theta, star_equat[star], star_polar[star]):
             #add entire row of star data from "stars" here to the current cluster
             data.append(stars.loc[star,:])
+        else:
+            pass
     #next line converts data list to Pandas data frame, with those column names which match the 'total point-like data.txt' file
     currentcluster = pd.DataFrame(data, columns=['Name', 'Equatorial', 'Polar', 'BlueFlux', 'GreenFlux', 'RedFlux', 'Parallax', 'RadialVelocity', 'Distance', 'Periodicity', 'Location'])
     clustername = 'X' + str(xcenter) + '-Y' + str(ycenter) + '-N' + str(len(data))  #according to naming convention in preamble
